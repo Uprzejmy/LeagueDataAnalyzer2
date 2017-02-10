c27c398cb1dea4b69186faf29d78c63ac4e94864
Uprzejmy <lmerynda@gmail.com>
2017-02-10 21:20:11 +0100

    HiddenSettings config simplified, external api connection example

---
+++ LeagueDataAnalyzer2/LeagueDataAnalyzer2/DataProvider/ApiConnector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueDataAnalyzer2.DataProvider
+{
+    class ApiConnector
+    {
+        public static string GetDataFromUrl(string url)
+        {
+            string xml = "";
+            
+            try
+            {
+                using (var webClient = new WebClient())
+                {
+                    xml = webClient.DownloadString(url);
+                }
+            }
+            catch(Exception e)
+            {
+                //add something to the log
+                throw (e);
+            }
+
+            return xml;
+        }
+    }
+}
--- LeagueDataAnalyzer2/LeagueDataAnalyzer2/ViewModel/ViewModelMain.cs
+++ LeagueDataAnalyzer2/LeagueDataAnalyzer2/ViewModel/ViewModelMain.cs
@@ -4,8 +4,10 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 using LeagueDataAnalyzer2.Model.Entity;
+using LeagueDataAnalyzer2.DataProvider;
 
 namespace LeagueDataAnalyzer2.ViewModel
 {
@@ -38,8 +40,9 @@ namespace LeagueDataAnalyzer2.ViewModel
                 new Match { Id=11, MatchId=12346 },
                 new Match { Id=12, MatchId=12347 },
             };
-            TextProperty1 = "Type here";
-            // TextProperty1 = System.Configuration.ConfigurationManager.AppSettings.Get("riot_key"); // broken somehow
+
+            string riotKey = ConfigurationManager.AppSettings.Get("riot_key");
+            TextProperty1 = ApiConnector.GetDataFromUrl("https://eune.api.pvp.net/api/lol/eune/v1.4/summoner/by-name/Uprzejmy?api_key=" + riotKey);
         }
     }
 }
