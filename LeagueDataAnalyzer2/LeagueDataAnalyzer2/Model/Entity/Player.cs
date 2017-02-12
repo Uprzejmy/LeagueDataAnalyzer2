65ba00ff47da572b10a2babf3d1c45675490a496
Uprzejmy <lmerynda@gmail.com>
2017-02-12 12:43:37 +0100

    Player entity added

---
+++ LeagueDataAnalyzer2/LeagueDataAnalyzer2/Model/Entity/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueDataAnalyzer2.Model.Entity
+{
+    class Player : INotifyPropertyChanged
+    {
+        int _id;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    RaisePropertyChanged("Id");
+                }
+            }
+        }
+
+        string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (_username != value)
+                {
+                    _username = value;
+                    RaisePropertyChanged("Username");
+                }
+            }
+        }
+
+
+        void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
