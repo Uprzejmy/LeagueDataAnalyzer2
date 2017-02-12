2d16b253dd55b248e12cd9d8b4c6a03f9cbdc6fa
Uprzejmy <lmerynda@gmail.com>
2017-02-12 12:44:02 +0100

    Simple storage level

---
+++ LeagueDataAnalyzer2/LeagueDataAnalyzer2/Model/Data.cs
@@ -0,0 +1,24 @@
+﻿using LeagueDataAnalyzer2.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueDataAnalyzer2.Model
+{
+    class Data
+    {
+        public IEnumerable<Match> Matches = new HashSet<Match>();
+        public IEnumerable<Player> Players = new HashSet<Player>();
+
+        private static Data data = null;
+
+        public static Data Instance { get { if (data == null) data = new Data(); return data; } }
+
+        private Data()
+        {
+
+        }
+    }
+}
