bb104ea3dbc5531696be9bb85dd8ef5b19b81449
Uprzejmy <lmerynda@gmail.com>
2017-02-12 12:43:51 +0100

    Model Repository base class

---
+++ LeagueDataAnalyzer2/LeagueDataAnalyzer2/Model/IRequestsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueDataAnalyzer2.Model
+{
+    interface IRequestsRepository
+    {
+        string GetPlayerByName(string name);
+        string GetMatchesByPlayerId(string id);
+    }
+}
---
+++ LeagueDataAnalyzer2/LeagueDataAnalyzer2/Model/RequestRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueDataAnalyzer2.Model
+{
+    class RequestRepository : IRequestsRepository
+    {
+        public string GetMatchesByPlayerId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPlayerByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
